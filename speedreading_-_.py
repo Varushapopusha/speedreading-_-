1d86db21d7c728e26d0d2ee749e5b85d0ccec712
Varushapopusha <varermoshina@gmail.com>
2023-03-08 13:02:09 +0300

    1

--- speedreading_-_.py
+++ speedreading_-_.py
@@ -1,19 +1,30 @@
 from play import*
 from time import*
-opros = new_text('начать',0,0,None,300,'black')
+opros = new_text('начать',0,0,None,200,'black')
 set_backdrop((153,0,0))
-with open('istoria-drevnego-mira.txt','r',encoding='UTF-8') as file:
+with open('-_royallib.com — копия.txt','r',encoding='ANSI') as file:
     data=file.read()
 data=data.split()
 print(data)
 q=0
+speed = 60
+reading = 1
 @repeat_forever
 def game():
     global q
-    opros.words=data[q]
-    sleep(0.1)
-    q+=1
-
+    global speed
+    global reading
+    if reading == 1:
+        opros.words=data[q]
+        sleep(60/speed)
+        q+=1
+    if key_is_pressed('q'):
+        reading *=(-1)
+    if key_is_pressed('w'):
+        speed += 50
+    if key_is_pressed('s'):
+        speed -= 50
+        
 
 
 
